25360b08055255664c6aac8f499631d2e55c2928
Max Daunarovich <max.daunarovich@gmail.com>
2022-04-22 18:09:33 +0300

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import Transaction from "../components/Transaction";
 import "../flow/config.js";
+import CadenceEditor from "../components/CadenceEditor";
+import { setEnvironment, executeScript } from "flow-cadut";
+
+const baseScript = `
+// This is the most basic script you can execute on Flow Network
+pub fun main():Int {
+  return 42
+}
+`.slice(1); // remove new line at the bof
 
 export default function Home() {
+  const [monacoReady, setMonacoReady] = useState(false);
+  const [code, updateCode] = useState(baseScript);
+
+  useEffect(() => {
+    // This is the way to setup current environment
+    setEnvironment("testnet");
+  }, []);
+
   return (
     <div>
       <Head>
@@ -13,9 +31,25 @@ export default function Home() {
 
       <main>
         <Transaction />
-        <h1>Hello!</h1>
-        <textarea>I love MaxStarka. Script code should go in here.</textarea>
-        <button>Execute Script</button>
+        <h1>Cadence Editor</h1>
+        {!monacoReady && <p>Please wait, instantiating Monaco Editor!</p>}
+        <CadenceEditor
+          onReady={() => setMonacoReady(true)}
+          code={code}
+          updateCode={updateCode}
+        />
+        <button
+          onClick={async () => {
+            const [result, executionError] = await executeScript({ code });
+            if (!executionError) {
+              console.log(result);
+            } else {
+              console.error(executionError);
+            }
+          }}
+        >
+          Execute Script
+        </button>
       </main>
     </div>
   );

5f1314beb3a43e95bf164d830cdb3160212477eb
Max Daunarovich <max.daunarovich@gmail.com>
2022-04-23 01:07:55 +0300

    Clean config file

--- next.config.js
+++ next.config.js
@@ -1,27 +1,19 @@
-const CopyPlugin = require("copy-webpack-plugin");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 const nextTranspileModules = require("next-transpile-modules");
-const path = require("path");
 const withTM = nextTranspileModules(["monaco-editor", "flow-cadut"]);
 
 module.exports = withTM({
   reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  webpack: (config, ) => {
+    config.resolve.alias["vscode"] = require.resolve(
+      "monaco-languageclient/lib/vscode-compatibility"
+    );
+
     config.plugins.push(
       new MonacoWebpackPlugin({
         languages: ["json"],
         features: [],
         filename: "static/[name].worker.js",
-      })
-    );
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: "node_modules/@onflow/cadence-language-server/dist/cadence-language-server.wasm",
-            to: "public/cadence-language-server.wasm",
-          },
-        ],
       })
     );
     return config;

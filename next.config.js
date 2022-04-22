64812bb7f07056bb34d88d751c33c56d04f1cbef
Max Daunarovich <max.daunarovich@gmail.com>
2022-04-22 18:09:00 +0300

    Update config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-module.exports = {
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin")
+const nextTranspileModules = require("next-transpile-modules")
+const withTM = nextTranspileModules(["monaco-editor", "flow-cadut"])
+
+module.exports = withTM({
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new MonacoWebpackPlugin({
+        languages: ["json"],
+        features: [],
+        filename: "static/[name].worker.js",
+      })
+    )
+    return config
+  }
+})

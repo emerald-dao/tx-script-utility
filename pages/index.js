7cd494c5931b108bdad0c758e4073985563b8632
Max Daunarovich <max.daunarovich@gmail.com>
2022-04-24 15:59:00 +0300

    More interaction refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import dynamic from "next/dynamic";
-import { executeScript, sendTransaction, getTemplateInfo } from "flow-cadut";
+import {
+  executeScript,
+  sendTransaction,
+  getTemplateInfo,
+  setEnvironment,
+  extendEnvironment,
+  getEnvironment,
+  extractImports,
+  replaceImportAddresses,
+} from "flow-cadut";
 
 import Transaction from "../components/Transaction";
 import CadenceEditor from "../components/CadenceEditor";
 
 import { useNetworkContext } from "../contexts/NetworkContext";
 import { buttonLabels } from "../templates/labels";
-import { baseTransaction } from "../templates/code";
+import { baseTransaction, flovatarTotalSupply } from "../templates/code";
 
 import * as fcl from "@onflow/fcl";
 
 import "../flow/config.js";
 import { configureForNetwork } from "../flow/config";
+import { debounce, fetchRegistry, prepareEnvironments } from "../utils";
 
 const CadenceChecker = dynamic(
   () => import("../components/LSP/CadenceChecker"),
@@ -33,30 +43,60 @@ const getButtonLabel = (type, signers = 0) => {
   return buttonLabels[type];
 };
 
+const getImports = debounce(async (code, setList) => {
+  const list = extractImports(code);
+  setList(list);
+}, 300);
+
+const prepareFinalImports = async (list, setFinal) => {
+  const env = await getEnvironment();
+  const replacedList = {};
+  for (const key in list) {
+    replacedList[key] = env[key];
+  }
+  const finalList = Object.keys(replacedList).map((key) => {
+    return {
+      name: key,
+      address: replacedList[key],
+    };
+  });
+
+  setFinal(finalList);
+};
+
 export default function Home() {
   const [monacoReady, setMonacoReady] = useState(false);
-  const [code, updateScriptCode] = useState(baseTransaction);
+  const [code, updateScriptCode] = useState(flovatarTotalSupply);
   const [result, setResult] = useState();
   const [user, setUser] = useState();
+  const [registry, setRegistry] = useState(null);
+  const [importList, setImportList] = useState({});
+  const [finalImports, setFinalImports] = useState([]);
 
-  const network = useNetworkContext();
+  const network = useNetworkContext() || "testnet";
 
   const templateInfo = getTemplateInfo(code);
   const { type, signers, args } = templateInfo;
 
-  // Method to send transactions and scripts
+  // METHDOS
+  const updateImports = async () => {
+    const env = await getEnvironment(network);
+    const newCode = replaceImportAddresses(code, env);
+    updateScriptCode(newCode);
+  };
   const send = async () => {
+    await setEnvironment(network);
+    extendEnvironment(registry);
+
     switch (true) {
       // Script Handling
       case type === "script": {
-        const [result, scriptError] = await executeScript({
-          code: code,
-        });
+        const [result, scriptError] = await executeScript({ code });
         if (!scriptError) {
           setResult(result);
         } else {
           setResult(scriptError);
-          console.log(scriptError);
+          console.error(scriptError);
         }
         break;
       }
@@ -69,14 +109,15 @@ export default function Home() {
         }
 
         const [txResult, txError] = await sendTransaction({
-          code: code,
-          limit: 9999
+          code,
+          limit: 9999,
+          payer: fcl.authz,
         });
         if (!txError) {
           setResult(txResult);
         } else {
           setResult(txError);
-          console.log(txError);
+          console.error(txError);
         }
         break;
       }
@@ -85,10 +126,31 @@ export default function Home() {
         break;
     }
   };
+  const getRegistry = async () => {
+    const data = await fetchRegistry();
+    const registry = prepareEnvironments(data);
+    extendEnvironment(registry);
+    setRegistry(registry);
+  };
 
+  // EFFECTS
   useEffect(() => {
+    fcl.unauthenticate();
     fcl.currentUser().subscribe(setUser);
+
+    getRegistry().then();
   }, []);
+  useEffect(() => {
+    setEnvironment(network);
+    if (registry !== null) {
+      extendEnvironment(registry);
+    }
+  }, [network]);
+  useEffect(() => getImports(code, setImportList), [code]);
+  useEffect(
+    () => prepareFinalImports(importList, setFinalImports),
+    [importList, network]
+  );
 
   const fclAble = signers && signers === 1 && type === "transaction";
   const disabled =
@@ -120,13 +182,38 @@ export default function Home() {
           </div>
         </CadenceChecker>
 
-        <button onClick={send} disabled={disabled}>
-          {getButtonLabel(type, signers)}
-        </button>
+        {monacoReady && (
+          <>
+            <div className="mb-1">
+              <h4>Replaced Addresses</h4>
+              <p>
+                These are the addresses, which will be used, before sending
+                Cadence template to network:
+              </p>
+              <ul>
+                {finalImports.map((item) => {
+                  const { name, address } = item;
+                  return (
+                    <li key={name}>
+                      <b>{name}</b> : {address}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
 
-        {fclAble ? (
-          <p className="note">✅ Transaction could be signed with FCL</p>
-        ) : null}
+            <div className="two-items-grid">
+              <button onClick={updateImports}>Update Imports</button>
+              <button onClick={send} disabled={disabled}>
+                {getButtonLabel(type, signers)}
+              </button>
+            </div>
+
+            {fclAble ? (
+              <p className="note">✅ Transaction could be signed with FCL</p>
+            ) : null}
+          </>
+        )}
 
         <h1>
           {result !== undefined && result !== null

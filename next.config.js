6c9b1c6bf34c365480c8877d2d19dd26bf1a93b6
Pratik Patel <prpatel05@gmail.com>
2022-08-05 01:24:46 -0400

    Try To Fix Next Build.

--- next.config.js
+++ next.config.js
@@ -3,19 +3,20 @@ const nextTranspileModules = require("next-transpile-modules");
 const withTM = nextTranspileModules(["monaco-editor", "flow-cadut"]);
 
 module.exports = withTM({
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias["vscode"] = require.resolve(
-      "monaco-languageclient/lib/vscode-compatibility"
-    );
+    reactStrictMode: true,
+    outputFileTracing: false,
+    webpack: (config) => {
+        config.resolve.alias["vscode"] = require.resolve(
+            "monaco-languageclient/lib/vscode-compatibility"
+        );
 
-    config.plugins.push(
-      new MonacoWebpackPlugin({
-        languages: ["json"],
-        features: [],
-        filename: "static/[name].worker.js",
-      })
-    );
-    return config;
-  },
+        config.plugins.push(
+            new MonacoWebpackPlugin({
+                languages: ["json"],
+                features: [],
+                filename: "static/[name].worker.js",
+            })
+        );
+        return config;
+    },
 });
